1883def9e1dc0f4c26961298b149b48d049ac531
philia1121 <101129234+philia1121@users.noreply.github.com>
2025-03-19 01:08:48 +0800

    CSVWriter Auto Naming Func

--- Petroller_Git/Assets/JC_Folder/JC_Script/Legacy/CSVWriter_Config.cs
+++ Petroller_Git/Assets/JC_Folder/JC_Script/Legacy/CSVWriter_Config.cs
@@ -1,16 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CSVWriter_Config : MonoBehaviour
 {
+    public NamingType namingType = NamingType.Custom;
     public string fileName;
     void Awake()
     {
-        if(fileName == null)
+        string fName = "DefaultFileName";
+        switch(namingType)
         {
-            fileName = "default";
+            case NamingType.Custom:
+                fName = (fileName == null)? "default" : fileName;
+                break;
+            case NamingType.TimeLog:
+                fName = System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+                break;
+            case NamingType.Guid:
+                fName = System.Guid.NewGuid().ToString();
+                break;
         }
-        RecordCSVWriter.CSV_SetFileName(fileName);
+        
+        RecordCSVWriter.CSV_SetFileName(fName);
     }  
+}
+
+[System.Serializable]
+public enum NamingType
+{
+    Custom,
+    TimeLog,
+    Guid,
 }
